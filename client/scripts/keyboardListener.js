09d0fd380af2e1d5e6478d20cc46eabbf7496429
Willian Guedes <willian.wg70@gmail.com>
2022-10-13 12:42:49 +0000

    move createKeyboardListener function to a separated file

---
+++ client/scripts/keyboardListener.js
@@ -0,0 +1,27 @@
+function createKeyboardListener(){
+    const state = {
+        observers:[]
+    }
+    function subscribe(observerFunction){
+        state.observers.push(observerFunction);
+    }
+
+    function notifyAll(command){
+        state.observers.map( observerFunction => observerFunction(command) );
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(event){
+        const command = {
+            playerId:'player1',
+            keyPressed:event.key
+        }
+      
+        notifyAll(command);
+    }
+
+    return {
+        subscribe
+    }
+}
