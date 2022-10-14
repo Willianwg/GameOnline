b32df37dd801f86beeb616af3fcb5592ee6e1189
Willian Guedes <willian.wg70@gmail.com>
2022-10-14 12:43:23 +0000

    change folders name, move index.html file and install socket.io

--- game/server.js
+++ game/server.js
@@ -11,4 +11,4 @@ const game = createGame();
 game.addPlayer({ playerId:'player1', playerX:4, playerY:8});
 game.addFruit({ fruitId:'fruit1', fruitX:12, fruitY:16});
 
-server.listen(3000);
+server.listen(3333);
