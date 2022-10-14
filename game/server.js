b572ad6de0cb85d823d198d7d9a436d10a8f88a3
Willian Guedes <willian.wg70@gmail.com>
2022-10-14 13:20:25 +0000

    add socket.io connection between server and client

--- game/server.js
+++ game/server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import http from 'http';
-import createGame from './scripts/createGame.js';
+import createGame from './public/scripts/createGame.js';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
-
 const server = http.createServer(app);
+const sockets = new Server(server);
+
 app.use(express.static('public'));
 
 const game = createGame();
 game.addPlayer({ playerId:'player1', playerX:4, playerY:8});
 game.addFruit({ fruitId:'fruit1', fruitX:12, fruitY:16});
+
+sockets.on('connection', (socket)=>{
+    console.log('did it!');
+})
 
 server.listen(3333);
